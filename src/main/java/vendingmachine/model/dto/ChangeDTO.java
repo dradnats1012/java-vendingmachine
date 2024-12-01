b55d35cb985d63a49068cd58736a6131c7ea8cc4
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 17:20:26 +0900

    feat: ChangeDTO 클래스 구현

---
+++ src/main/java/vendingmachine/model/dto/ChangeDTO.java
@@ -0,0 +1,23 @@
+package vendingmachine.model.dto;
+
+import java.util.Map;
+
+import vendingmachine.model.Change;
+
+public class ChangeDTO {
+
+    private final Map<Integer, Integer> changes;
+
+
+    public ChangeDTO(Change change){
+        this.changes = change.getChanges();
+    }
+
+    public static ChangeDTO from(Change change){
+        return new ChangeDTO(change);
+    }
+
+    public Map<Integer, Integer> getChanges(){
+        return changes;
+    }
+}
