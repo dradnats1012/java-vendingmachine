34896e4d7b7eb46f68bd0b4bcf7729184d3fbbfe
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 16:33:40 +0900

    feat: Money 클래스 구현

---
+++ src/main/java/vendingmachine/model/Money.java
@@ -0,0 +1,36 @@
+package vendingmachine.model;
+
+import static vendingmachine.util.ErrorMessage.*;
+
+public class Money {
+
+    private int money;
+
+    public Money(String inputMoney) {
+        this.money = parseToInteger(inputMoney);
+        validateNegative();
+    }
+
+    private int parseToInteger(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MONEY_ONLY_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNegative(){
+        if(money < 0){
+            throw new IllegalArgumentException(ERROR_NEGATIVE.getMessage());
+        }
+    }
+
+    public int getMoney(){
+        return money;
+    }
+
+    public void minusMoney(int minus){
+        money -= minus;
+    }
+}
+
