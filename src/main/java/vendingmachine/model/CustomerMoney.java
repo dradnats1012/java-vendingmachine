bd0f6f84dafdf3747014cd2908dc6c83b6138fa3
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 16:35:58 +0900

    feat: CustomerMoney 클래스 구현

---
+++ src/main/java/vendingmachine/model/CustomerMoney.java
@@ -0,0 +1,36 @@
+package vendingmachine.model;
+
+import static vendingmachine.util.ErrorMessage.*;
+
+public class CustomerMoney {
+
+    private int customerMoney;
+
+    public CustomerMoney(String inputMoney) {
+        this.customerMoney = parseToInteger(inputMoney);
+        validateNegative();
+    }
+
+    private int parseToInteger(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MONEY_ONLY_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNegative(){
+        if(customerMoney < 0){
+            throw new IllegalArgumentException(ERROR_NEGATIVE.getMessage());
+        }
+    }
+
+    public int getMoney(){
+        return customerMoney;
+    }
+
+    public void minusMoney(int minus){
+        customerMoney -= minus;
+    }
+}
+
