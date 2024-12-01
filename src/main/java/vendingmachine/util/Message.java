f2d90f0d969c28dac90787818c08665dc83cd206
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 13:22:31 +0900

    feat: Message 열거형 생성

---
+++ src/main/java/vendingmachine/util/Message.java
@@ -0,0 +1,22 @@
+package vendingmachine.util;
+
+public enum Message {
+
+    INPUT_VENDING_MACHINE_MONEY("자판기가 보유하고 있는 금액을 입력해 주세요."),
+    VENDING_MACHINE_COIN("자판기가 보유한 동전"),
+    INPUT_GOODS("상품명과 가격, 수량을 입력해 주세요."),
+    INPUT_MONEY("투입 금액을 입력해 주세요."),
+    MONEY("투입 금액: %d원"),
+    CHANGE("잔돈")
+    ;
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
