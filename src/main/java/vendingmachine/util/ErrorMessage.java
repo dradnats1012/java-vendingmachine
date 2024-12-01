176730adafa14e251adc03c69a2ab58b0f731c24
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 16:33:25 +0900

    feat: ErrorMessage 클래스 구현

---
+++ src/main/java/vendingmachine/util/ErrorMessage.java
@@ -0,0 +1,23 @@
+package vendingmachine.util;
+
+public enum ErrorMessage {
+
+    ERROR_MONEY_ONLY_NUMBER("금액은 숫자여야 합니다."),
+    ERROR_PRICE_MORE_THAN_100("가격은 100원 이상이어야 합니다."),
+    ERROR_PRICE_UNIT_10("가격은 10원 단위이어야 합니다."),
+    ERROR_PRICE_ONLY_NUMBER("가격은 숫자여야 합니다."),
+    ERROR_NEGATIVE("양수를 입력해주세요."),
+    ERROR_AMOUNT_ONLY_NUMBER("수량은 숫자여야 합니다.")
+
+    ;
+
+    private final String message;
+
+    ErrorMessage(String message){
+        this.message = "[ERROR] " + message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
--- src/main/java/vendingmachine/util/Message.java
+++ src/main/java/vendingmachine/util/Message.java
@@ -4,10 +4,13 @@ public enum Message {
 
     INPUT_VENDING_MACHINE_MONEY("자판기가 보유하고 있는 금액을 입력해 주세요."),
     VENDING_MACHINE_COIN("자판기가 보유한 동전"),
+    VENDING_MACHINE_COIN_COUNT("%d원 - %d개"),
     INPUT_GOODS("상품명과 가격, 수량을 입력해 주세요."),
     INPUT_MONEY("투입 금액을 입력해 주세요."),
+    PURCHASE_GOODS("구매할 상품명을 입력해 주세요."),
     MONEY("투입 금액: %d원"),
     CHANGE("잔돈")
+
     ;
 
     private final String message;
