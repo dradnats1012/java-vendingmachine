daa293e475c2bce47b262b93755e97a9b274724a
dradnats1012 <herjebi1012@gmail.com>
2024-12-01 17:21:24 +0900

    fix: OutputView 클래스 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,17 +1,36 @@
 package vendingmachine.view;
 
-import static vendingmachine.util.Message.CHANGE;
-import static vendingmachine.util.Message.VENDING_MACHINE_COIN;
+import static vendingmachine.util.Message.*;
+
+import java.util.Map;
+
+import vendingmachine.model.dto.ChangeDTO;
+import vendingmachine.model.dto.CoinDTO;
 
 public class OutputView {
 
-    public static void printCoin(){
+    public static void printCoin(CoinDTO coinDTO){
         System.out.println(VENDING_MACHINE_COIN.getMessage());
-
+        for (Map.Entry<Integer, Integer> entry : coinDTO.getCoinCount().entrySet()) {
+            System.out.printf(VENDING_MACHINE_COIN_COUNT.getMessage() + "\n", entry.getKey(), entry.getValue());
+        }
+        System.out.println();
     }
 
-    public static void printChange(){
-        System.out.println(CHANGE.getMessage());
+    public static void printRemainderMoney(int money){
+        System.out.println();
+        System.out.printf(MONEY.getMessage() + "\n", money);
+    }
 
+    public static void printChange(ChangeDTO changeDTO){
+        System.out.println(CHANGE.getMessage());
+        for (Map.Entry<Integer, Integer> entry : changeDTO.getChanges().entrySet()) {
+            System.out.printf(VENDING_MACHINE_COIN_COUNT.getMessage() + "\n", entry.getKey(), entry.getValue());
+        }
+        System.out.println();
+    }
+
+    public static void printException(String message){
+        System.out.println(message);
     }
 }
